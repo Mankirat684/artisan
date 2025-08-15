d0f2ad9602783807dcb57df923c85d53e54b2fb7
Mankirat684 <mankirats213@gmail.com>
2025-08-15 20:37:13 +0530

    Add user routes to the application for user-related API endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
+//routes
+import userRouter from './routes/user.routes.js'
+app.use('/api/v1/users', userRouter);
 
 export default app;
